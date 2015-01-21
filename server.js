bd854fc21b574f05b8168ef8e48e3a1420121063
Nathan Miller <nmiller014@gmail.com>
2015-01-20 23:05:47 -0600

    Added basic server code

---
+++ server.js
@@ -0,0 +1,9 @@
+var zetta = require('zetta');
+var MOTOR = require('devices/motor/index');
+
+zetta()
+  .name('FirstName-LastName')
+  .use(MOTOR, [ "P9_14", "P9_16" ], false)
+  .listen(1337, function(){
+     console.log('Zetta is running at http://127.0.0.1:1337');
+});

eec8b83289ab39aedb12cf495c7936233de746a8
Nathan Miller <nmiller014@gmail.com>
2015-01-21 21:12:25 -0600

    Fixed some issues in server, added second motor, and switched over to using autoscout

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 var zetta = require('zetta');
-var MOTOR = require('./devices/motor/motor_driver');
+var MOTOR = require('./devices/motor/index');
 
 zetta()
   .name('FirstName-LastName')
-  .use(MOTOR, [ "P9_14", "P9_16" ], false)
+  .use(MOTOR, [ "P9_14", "P9_16" ], true)
+  .use(MOTOR, [ "P8_13", "P8_19" ], true)
   .listen(1337, function(){
      console.log('Zetta is running at http://127.0.0.1:1337');
 });

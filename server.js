fbc66cd08cde8d6d98a3d3557d29d1e9c3cb0d25
Nathan Miller <nmiller014@gmail.com>
2015-01-30 02:40:39 +0000

    Added battery to server.js

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 var zetta = require('zetta');
 var MOTOR = require('./devices/motor/index');
+var BATTERY = require('./devices/battery/index');
 
 zetta()
   .name('FirstName-LastName')
   .use(MOTOR, [ "P9_14", "P9_16" ], true)
   .use(MOTOR, [ "P8_13", "P8_19" ], true)
+  .use(BATTERY, 'P9_40', 12.3, 10, 25)
   .listen(1337, function(){
      console.log('Zetta is running at http://127.0.0.1:1337');
 });
